09c05eff92b6b694d935288286d46b370070a285
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2021-09-19 17:20:00 +0300

    part 6 - new controller generators costomization with no block docs

---
+++ app/Http/Controllers/Admin/PageController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Page;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+
+    public function index(): View
+    {
+        $pages = Page::all();
+        return view('pages.index', compact('pages'));
+    }
+
+    public function create(): View
+    {
+        return view('pages.create');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        return redirect()->route('pages.index');
+    }
+
+    public function show(Page $page): View
+    {
+        return view('pages.show', compact('page'));
+    }
+
+    public function edit(Page $page): View
+    {
+        return view('pages.edit', compact('page'));
+
+    }
+
+    public function update(Request $request, Page $page): RedirectResponse
+    {
+       return redirect()->route('pages.index');
+    }
+
+    public function destroy(Page $page): RedirectResponse
+    {
+        return redirect()->route('pages.index');
+    }
+}
--- app/Http/Controllers/Admin/TaskController.php
+++ app/Http/Controllers/Admin/TaskController.php
@@ -6,88 +6,31 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreTaskRequest;
 use App\Models\Checklist;
 use App\Models\Task;
-use Illuminate\Http\Request;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class TaskController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  StoreTaskRequest $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreTaskRequest $request, Checklist $checklist)
+    public function store(StoreTaskRequest $request, Checklist $checklist): RedirectResponse
     {
         $position = $checklist->tasks()->max('position') + 1;
         $checklist->tasks()->create($request->validated() + ['position' => $position]);
         return redirect()->route('admin.checklist_groups.checklists.edit', [$checklist->checklist_group_id, $checklist]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  Checklist $checklist,
-     * @param  Task $task
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Checklist $checklist, Task $task)
+    public function edit(Checklist $checklist, Task $task): View
     {
         return view('admin.tasks.edit', compact('checklist', 'task'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  StoreTaskRequest $request
-     * @param  Checklist $checklist
-     * @param  Task $task
-     * @return \Illuminate\Http\Response
-     */
-    public function update(StoreTaskRequest $request, Checklist $checklist, Task $task)
+    public function update(StoreTaskRequest $request, Checklist $checklist, Task $task): RedirectResponse
     {
         $task->update($request->validated());
         return redirect()->route('admin.checklist_groups.checklists.edit', [$checklist->checklist_group_id, $checklist]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  Checklist $checklist
-     * @param  Task $task
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Checklist $checklist, Task $task)
+    public function destroy(Checklist $checklist, Task $task): RedirectResponse
     {
         // for the sake of the position order
         $checklist->tasks()->where('position', '>', $task->position)->decrement('position');
